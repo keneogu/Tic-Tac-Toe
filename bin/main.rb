67151150e15e3aba69f4eb80bf3192826c0d859f
ZawadiSumba66 <rosesumba98@gmail.com>
2021-01-12 17:11:52 +0300

    change  the players interface

--- bin/main.rb
+++ bin/main.rb
@@ -2,25 +2,27 @@ puts 'Kene-Rose Tic_Tac_Toe game'
 puts
 
 players = []
-
-def user_validity(username)
-    puts "Enter username"
-    username=gets.chomp()
-    while username.empty?
-        puts "Username required"
-        username=gets.chomp()
+    (0...2).each do |i|
+    puts "Enter name of Player #{i+1}"
+    player=gets.chomp()
+    i+=1
+    players.push(player)
+    while player.empty?
+        puts  "Name of player required "
+        player=gets.chomp() 
     end
-    username
 end
-p player1 =user_validity("x")
-p player2 =user_validity("y")
 
-layer = [" "," "," "," "," "," "," "," "," "]
+
+play = [[" "," "," "],[" "," "," "],[" "," "," "]]
  
-puts " 1    2    3"
-puts "|#{layer[0]}|  |#{layer[1]}|  |#{layer[2]}|"
+puts '   1 2 3 '
+puts '  -------'
+puts "a |#{play[0][0]}|#{play[0][1]}|#{play[0][2]}|"
+puts '  -------'
+puts "b |#{play[1][0]}|#{play[1][1]}|#{play[1][2]}|"
+puts '  -------'
+puts "c |#{play[2][0]}|#{play[2][1]}|#{play[2][2]}|"
+puts '  -------'
 puts
-puts "|#{layer[3]}|  |#{layer[4]}|  |#{layer[5]}|"
-puts
-puts "|#{layer[6]}|  |#{layer[7]}|  |#{layer[8]}|"
 
