80a22610f3bcb889bcd00ec58bcd795633ad0d9a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-14 16:47:19 +0100

    modified the player flow in the game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,21 +1,29 @@
 # !/usr/bin/env ruby
 
 require_relative '../lib/player'
+require_relative './board'
 
-player_x = Player.new
-player_x.sign = 'X'
-puts 'Enter user X name'
-player_x.name = gets.chomp
-while player_x.name == ''
-  puts 'Player name is required'
+  puts "Welcome to Kene-Rose TIC-TAC-TOE game"
+  layout = Board.new
+
+  puts layout.display_board1
+  puts 
+  player_x = Player.new("player1","X")
+  puts 'Enter player 1 name'
   player_x.name = gets.chomp
-end
+  puts "welcome #{player_x.name} you're player 1"
+  while player_x.name == ''
+    puts 'Player name is required'
+    player_x.name = gets.chomp
+  end
+  puts
 
-player_o = Player.new
-player_o.sign = 'O'
-puts 'Enter user O name'
-player_o.name = gets.chomp
-while player_o.name == '' || player_o.name == player_x.name
-  puts 'Player name is required or invalid name'
+  player_o = Player.new("player2","O")
+  puts 'Enter player 2 name'
   player_o.name = gets.chomp
-end
+  puts "welcome #{player_o.name} you're player 2"
+  while player_o.name == '' || player_o.name == player_x.name
+    puts 'Player name is required or invalid name'
+    player_o.name = gets.chomp
+  end
+  puts
