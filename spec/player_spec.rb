8c9c672c91a78e3adfb79bc4fee31b0c44324acb
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-21 11:07:22 +0100

    Test player.rb

---
+++ spec/player_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require_relative '../lib/player'
+
+describe Player do
+  let(:player1) { Player.new('Kene', 'X') }
+  let(:player2) { Player.new('Rose', 'O') }
+
+  context '#initialize' do
+    it 'creates the player1 name and sign' do
+      expect(player1.name).to eq('Kene')
+      expect(player1.sign).to eq('X')
+    end
+
+    it 'creates the player2 name and sign' do
+      expect(player2.name).to eq('Rose')
+      expect(player2.sign).to eq('O')
+    end
+  end
+end
